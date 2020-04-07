737447a4a7f2e429abdcf7af562a068e5525af23
Stephan Kulla <git.mail@kulla.me>
2020-04-07 13:56:26 +0200

    Add test.sh to test script

---
+++ container/learning-analytics/test.sh
@@ -0,0 +1,10 @@
+curl -X POST \
+  http://localhost:8090/api/notebook/run/2EZMATNCU/20200109-205741_1474479729 \
+  -H 'Content-Type: application/json' \
+  -H 'Postman-Token: 0995608a-e851-441f-9af2-035e2817b2ee' \
+  -H 'cache-control: no-cache' \
+  -d '{
+    "params": {
+        "user_id": "1"
+    }
+}'
